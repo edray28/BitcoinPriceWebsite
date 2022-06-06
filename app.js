17fd75aa7ebb20f561508c5a1bcc01251ef73041
edray28 <edraytuyor@gmail.com>
2022-06-06 16:04:08 +0800

    Added New API

--- app.js
+++ app.js
@@ -4,22 +4,21 @@ const myLink = document.querySelector('.gitico');
 const createdBy = document.createElement('h5');
 const myDataCol = document.querySelectorAll('td');
 
-// API 
+//API
 const fetchBitcoinAPI = async () => {
     try {
-        const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd');
-        const data = await res.json();  
-        myDataCol[0].innerHTML = data.ticker.base; 
-        myDataCol[1].innerHTML = data.ticker.target +"$";  
-        myDataCol[2].innerHTML = "$"+data.ticker.price; 
-        myDataCol[3].innerHTML = data.ticker.volume; 
-        myDataCol[4].innerHTML = data.ticker.change; 
+        const res = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true");
+        const data = await res.json();
+        myDataCol[0].innerHTML = "$"+ data.bitcoin.usd;
+        myDataCol[1].innerHTML = "$"+ data.bitcoin.usd_market_cap;
+        myDataCol[2].innerHTML = "$"+ data.bitcoin.usd_24h_vol;
+        myDataCol[3].innerHTML = "$"+ data.bitcoin.usd_24h_change; 
+        myDataCol[4].innerHTML = data.bitcoin.last_updated_at; 
     } catch (e) {
         bitTitle.innerText = "BITCOIN API NOT AVAILABLE RIGHT NOW!";
         console.log(e);
     }
 }
-
 
 //Other Dev
 const showDev = () => {
